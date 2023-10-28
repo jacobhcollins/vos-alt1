db25f185a293f2fa00a914a0699d99ad6f5c7b31
Jacob Collins <Jakehunter27+gh@gmail.com>
2023-10-28 15:42:44 -0600

    It works now ;)

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 logging.basicConfig(level=logging.DEBUG)
 
-ip_addresses_voted  = []
+ip_addresses_voted  = set()
 clan_counters = {
     'iorwerth': 0,
     'amlodd': 0,
@@ -21,6 +21,21 @@ clan_counters = {
     'trahaearn': 0
 }
 counter_lock = threading.Lock()
+
+def add_csp_headers(response):
+    response.headers.add('Content-Security-Policy', "default-src 'self'; content-src *")
+    if request.method == 'OPTIONS':
+        options_resp = make_response()
+        options_resp.headers.add('Access-Control-Allow-Origin', '*')  
+        options_resp.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+        options_resp.headers.add('Access-Control-Allow-Methods', '*')
+        return options_resp
+    return response
+
+@app.after_request
+def apply_csp(response):
+    return add_csp_headers(response)
+
 
 @app.route('/increase_counter', methods=['POST'])
 def increase_counter():
@@ -39,22 +54,18 @@ def increase_counter():
                             response[clan] = clan_counters[clan]
                         else:
                             response[clan] = 'Invalid clan'
-                ip_addresses_voted.append(client_ip)
+                ip_addresses_voted.add(client_ip) 
                 response = jsonify(response)
-                response.headers.add('Content-Security-Policy', "default-src 'self' *")
                 return response
             else:
                 response = jsonify({'error': 'Please provide a list of exactly two clans'})
-                response.headers.add('Content-Security-Policy', "default-src 'self' *")
                 return make_response(response, 400)
         else:
             logging.info(f'{client_ip} hit rate limit.')
             response = jsonify({'error': 'Rate limited, try again in an hour.'})
-            response.headers.add('Content-Security-Policy', "default-src 'self' *")
             return make_response(response, 400)
     else:
         response = jsonify({'error': 'Invalid JSON'})
-        response.headers.add('Content-Security-Policy', "default-src 'self' *")
         return make_response(response, 400)
 
 @app.route('/vos', methods=['GET'])
@@ -62,25 +73,27 @@ def vos():
     with counter_lock:
         sorted_counts = sorted(clan_counters.items(), key=lambda x: x[1], reverse=True)
         top_2 = [clan[0] for clan in sorted_counts[:2] if clan[1] != 0]
-        if len(top_2) == 2:
-            return jsonify(top_2)
-        else:
-            return jsonify(None)
+        logging.info(f'Top 2 clans requested')
+        response = {f"clan_{index + 1}": clan for index, clan in enumerate(top_2)}
+        return make_response(jsonify(response), 200)
 
 @app.route('/counts', methods=['GET'])
 def counts():
     with counter_lock:
+        client_ip = request.remote_addr
         sorted_counts = sorted(clan_counters.items(), key=lambda x: x[1], reverse=True)
-        return jsonify(sorted_counts)
+        logging.info(f'Counts given to {client_ip}')
+        response = {f"count_{index + 1}": {clan[0]: clan[1]} for index, clan in enumerate(sorted_counts)}
+        return make_response(jsonify(response), 200)
     
 @app.route('/reset', methods=['GET'])   
 def reset_counters():
     global ip_addresses_voted 
-    ip_addresses_voted = []
+    ip_addresses_voted = set()
     for clan in clan_counters:
         clan_counters[clan] = 0
     logging.info("Counters and IP logging reset at " + time.ctime())
-    return make_response(jsonify({'Reset app manually'}), 400)
+    return make_response(jsonify({'message': 'Counters and IP logging reset'}), 200)
 
 schedule.every().hour.at(":00").do(reset_counters)
 
