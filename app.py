3ddbdbfafca4f0ace0e0d5d08097e47bf0c3b5dd
Jacob Collins <Jakehunter27+gh@gmail.com>
2023-10-28 18:33:59 -0600

    added last_vos

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 logging.basicConfig(level=logging.DEBUG)
 
-ip_addresses_voted  = set()
+last_vos = {}
 clan_counters = {
     'iorwerth': 0,
     'amlodd': 0,
@@ -20,16 +20,19 @@ clan_counters = {
     'meilyr': 0,
     'trahaearn': 0
 }
+
 counter_lock = threading.Lock()
 
 def add_csp_headers(response):
     response.headers.add('Content-Security-Policy', "default-src 'self'; content-src *")
+    
     if request.method == 'OPTIONS':
         options_resp = make_response()
         options_resp.headers.add('Access-Control-Allow-Origin', '*')  
         options_resp.headers.add('Access-Control-Allow-Headers', 'Content-Type')
         options_resp.headers.add('Access-Control-Allow-Methods', '*')
         return options_resp
+    
     return response
 
 @app.after_request
@@ -42,27 +45,20 @@ def increase_counter():
     global ip_addresses_voted
     if request.is_json:
         data = request.get_json()
-        client_ip = request.remote_addr
-        if client_ip not in ip_addresses_voted:
-            if 'clans' in data and isinstance(data['clans'], list) and len(data['clans']) == 2:
-                response = {}
-                with counter_lock:
-                    for clan in data['clans']:
-                        if clan in clan_counters:
-                            clan_counters[clan] += 1
-                            logging.info(f'SUCCESS, {clan} count: {clan_counters[clan]}')
-                            response[clan] = clan_counters[clan]
-                        else:
-                            response[clan] = 'Invalid clan'
-                ip_addresses_voted.add(client_ip) 
-                response = jsonify(response)
-                return response
-            else:
-                response = jsonify({'error': 'Please provide a list of exactly two clans'})
-                return make_response(response, 400)
+        if 'clans' in data and isinstance(data['clans'], list) and len(data['clans']) == 2:
+            response = {}
+            with counter_lock:
+                for clan in data['clans']:
+                    if clan in clan_counters:
+                        clan_counters[clan] += 1
+                        logging.info(f'SUCCESS, {clan} count: {clan_counters[clan]}')
+                        response[clan] = clan_counters[clan]
+                    else:
+                        response[clan] = 'Invalid clan'
+            response = jsonify(response)
+            return response
         else:
-            logging.info(f'{client_ip} hit rate limit.')
-            response = jsonify({'error': 'Rate limited, try again in an hour.'})
+            response = jsonify({'error': 'Please provide a list of exactly two clans'})
             return make_response(response, 400)
     else:
         response = jsonify({'error': 'Invalid JSON'})
@@ -80,20 +76,31 @@ def vos():
 @app.route('/counts', methods=['GET'])
 def counts():
     with counter_lock:
-        client_ip = request.remote_addr
         sorted_counts = sorted(clan_counters.items(), key=lambda x: x[1], reverse=True)
-        logging.info(f'Counts given to {client_ip}')
-        response = {f"count_{index + 1}": {clan[0]: clan[1]} for index, clan in enumerate(sorted_counts)}
+        logging.info(f'Counts given.')
+        response = {f"now: count_{index + 1}": {clan[0]: clan[1]} for index, clan in enumerate(sorted_counts)}
         return make_response(jsonify(response), 200)
-    
+
+@app.route('/last_vos', methods=['GET'])
+def get_last_vos():
+    if last_vos:
+        return make_response(jsonify(last_vos), 200)
+    else:
+        return make_response(jsonify({'message': 'No previous VOS data'}), 404)
+        
 @app.route('/reset', methods=['GET'])   
 def reset_counters():
-    global ip_addresses_voted 
-    ip_addresses_voted = set()
+    global last_vos
+    with counter_lock:
+        last_vos = {}
+        sorted_counts = sorted(clan_counters.items(), key=lambda x: x[1], reverse=True)
+        top_2 = [clan[0] for clan in sorted_counts[:2] if clan[1] != 0]
+        for index, clan in enumerate(top_2):
+            last_vos[f"clan_{index + 1}"] = clan
     for clan in clan_counters:
         clan_counters[clan] = 0
-    logging.info("Counters and IP logging reset at " + time.ctime())
-    return make_response(jsonify({'message': 'Counters and IP logging reset'}), 200)
+    logging.info("Counters reset at " + time.ctime())
+    return make_response(jsonify({'message': 'Counters reset'}), 200)
 
 schedule.every().hour.at(":00").do(reset_counters)
 
